3b6a66c1922905063847d76b966c0148e6ef69b6
omprakashkumar <omkumar14031994@gmail.com>
2022-11-05 18:19:16 +0530

    First commit

---
+++ Demo.java
@@ -0,0 +1,15 @@
+class Test
+{
+int a=10; //Instance variable.
+static int b=20; //Static variable.
+public static void main (String[] arg)
+{
+int c=30; //Local variable.
+final int D=40; //final variable.
+Test t=new Test ();//object creation
+system.out.println(t.a);
+system.out.println(b);
+system.out.println(c);
+system.out.println(D);
+}
+}
